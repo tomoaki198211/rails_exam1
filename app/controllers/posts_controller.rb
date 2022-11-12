2118d792d49af14f3656b165b50fa6a797b36293
tomoaki_narukawa <tomo111911111@gmail.com>
2022-11-12 11:36:11 +0900

    contentに140文字以下のバリデーションを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました！"
+    else
+      render :new
+    end
   end
 
   private
