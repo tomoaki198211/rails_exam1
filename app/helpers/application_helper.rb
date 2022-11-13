6ac4e464bdd43a2ed2f234d47e97b7e88c29170c
tomoaki_narukawa <tomo111911111@gmail.com>
2022-11-13 10:02:07 +0900

    posts/index.htmlの画面を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  BASE_HEADER_TITLE = "ホーム".freeze
+  def header_title(page_title)
+     if page_title.blank?
+      BASE_HEADER_TITLE
+     else
+      "#{page_title}"
+     end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ module RailsExam1
     config.load_defaults 6.1
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
